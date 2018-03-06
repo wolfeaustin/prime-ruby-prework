77ac67116c1f748b07e14f37cc9617f047adcf0b
Austin Wolfe <wolfe_austin@yahoo.com>
2018-03-06 00:01:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+  return false if num <= 1 
+  primed = true 
+  i = 1
+  while i < num -1
+    i += 1 
+    if(num%i == 0 )
+      primed = false 
+    end
+  end 
+  
+  primed 
+end 
